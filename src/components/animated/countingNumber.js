646958d22a9d08b96741bb22a6766f1c1e3222ee
Obodo Bright <obodobright0@gmail.com>
2024-02-25 19:20:12 +0100

    bugFix: Removed deprecated dependency to activate deploymenr

--- src/components/animated/countingNumber.js
+++ src/components/animated/countingNumber.js
@@ -1,16 +1,17 @@
-export const countingNumber = () => {
-  return (
-    <AnimatedNumber
-      component="text"
-      value={bigValue}
-      style={{
-        transition: "0.8s ease-out",
-        fontSize: 48,
-        transitionProperty: "background-color, color, opacity",
-      }}
-      frameStyle={(perc) => (perc === 100 ? {} : { backgroundColor: "#ffeb3b" })}
-      duration={300}
-      formatValue={(n) => prettyBytes(n)}
-    />
-  );
-};
+// export const countingNumber = () => {
+//   return (
+//     <AnimatedNumber
+//       component="text"
+//       value={bigValue}
+//       style={{
+//         transition: "0.8s ease-out",
+//         fontSize: 48,
+//         transitionProperty: "background-color, color, opacity",
+//       }}
+//       frameStyle={(perc) => (perc === 100 ? {} : { backgroundColor: "#ffeb3b" })}
+//       duration={300}
+//       formatValue={(n) => prettyBytes(n)}
+//     />
+//   );
+// };
+// //
